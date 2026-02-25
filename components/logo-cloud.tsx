ef9eff469e3b3e151fab736e1293d1cc42e29ec0
Yogesh Vashisth <writetokhair@gmail.com>
2026-02-26 00:44:04 +0530

    logo changed

--- components/logo-cloud.tsx
+++ components/logo-cloud.tsx
@@ -3,13 +3,15 @@ export default function LogoCloud() {
         <section className="bg-background py-16">
             <div className="mx-auto max-w-5xl px-6">
                 <h2 className="text-center text-lg font-medium">Your favorite companies are our partners.</h2>
-                <div className="mx-auto mt-20 flex max-w-4xl flex-wrap items-center justify-center gap-x-12 gap-y-8 sm:gap-x-16 sm:gap-y-12">
+                <div className="mx-auto mt-20 flex max-w-5xl flex-wrap items-center justify-center gap-x-12 gap-y-8 sm:gap-x-16 sm:gap-y-12">
                     <img className="h-20 w-fit dark:invert" src="https://upload.wikimedia.org/wikipedia/en/2/22/Varun_Beverages.svg" alt="Varun" height="50" width="auto" />
                     <img className="h-20 w-fit dark:invert" src="https://media.licdn.com/dms/image/v2/C510BAQEbdd8enLx_Ng/company-logo_200_200/company-logo_200_200/0/1630569322018/tigmoozm_logo?e=2147483647&v=beta&t=OVuOZ5GLUKbt6__NMQgNGWaBzhDNdq5NOz05ObyCHPM" alt="Tigmoo logo" height="16" width="auto" />
                     <img className="h-20 w-fit dark:invert" src="https://www.apax.com/media/1785/_0093_unilabs.png?rmode=max&width=800&height=400&saturation=0" alt="Unilabs logo" height="16" width="auto" />
                     <img className="h-20 w-fit dark:invert" src="https://dmaxdentalcenter.com/wp-content/uploads/2024/11/D-MAX-Final-Logo-2-1.svg" alt="Unilabs logo" height="16" width="auto" />
                     <img className="h-20 w-fit dark:invert" src="https://www.tigmooeats.com/assets/web/img/logo.png" alt="Unilabs logo" height="16" width="auto" />
                     <img className="h-20 w-fit dark:invert" src="/pnache.jpeg" alt="Pnache" height="16" width="auto" />
+                    <img className="h-20 w-fit dark:invert" src="https://www.mercury.co.zm/wp-content/uploads/2018/10/logo-300.png" alt="mercury logistics" height="16" width="auto" />
+                    <img className="h-20 w-fit dark:invert rounded-full" src="/damax.jpeg" alt="mercury logistics" height="16" width="auto" />
 
                 </div>
             </div>
